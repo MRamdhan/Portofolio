c1f4c7f8e76b02a399a73ec10dcf75185c8a23d8
admin <daniiidani883@gmail.com>
2023-08-19 01:32:40 +0700

    perbaikan

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(router)
 
-app.mount('#app');
+app.mount('#app')
